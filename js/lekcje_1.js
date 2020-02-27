8d48c11e20dfef492070dc822fa589457329a33f
Bartek Ciesielski <bartlomiej-ciesielski@wp.pl>
2020-02-27 10:16:37 +0100

    add shotAtAngle animation in css

---
+++ js/lekcje_1.js
@@ -0,0 +1,14 @@
+
+
+console.log("start")
+
+const add = (a,b) => {
+    if(typeof(a) !== "number" & typeof(b) !== "number"){console.log("Niepoprawne parametry"); return};
+    if(a&b){console.log(a+b)};
+    if(!b) {console.log(a)};
+ }
+
+
+ console.log("if any input diffrent then number returns alert", add("f"));
+ console.log("if inputs are two numbers returns sum", add(2,3));
+ console.log("if only one number returns this number", add(3));
