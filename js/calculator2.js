2f4cca04b85ff317fb254cc9b6847564189d7e5c
Borys Maciuch <borysmaciuch@gmail.com>
2020-03-05 20:10:57 +0100

    calculator done

--- js/calculator2.js
+++ js/calculator2.js
@@ -5,76 +5,46 @@ let finalPrice = slider.value * 10;
 const firstDiscountValue = 0.1;
 const secondDiscountValue = 0.2;
 const thirdDiscountValue = 0.3;
+let discount = 0;
 const discount1 = document.querySelector("#disc1");
 
 const discount2 = document.querySelector("#disc2");
 
 const discount3 = document.querySelector("#disc3");
+const suggestions = document.querySelector(".suggestions");
+suggestions.innerHTML = slider.value;
 calculatorPrice.innerHTML = `${finalPrice}$`;
-function priceFunction(x, y) {
-  calculatorPrice.innerHTML = `${finalPrice}$ ${x} ${y}`;
+function priceFunction() {
+  if (discount1.checked && discount2.checked && discount3.checked) {
+    discount =
+      Number(slider.value) +
+      Number(slider.value) * 2 +
+      Number(slider.value) * 3;
+  } else if (discount1.checked && discount2.checked) {
+    discount = Number(slider.value) + Number(slider.value) * 2;
+  } else if (discount1.checked && discount3.checked) {
+    discount = Number(slider.value) + Number(slider.value) * 3;
+  } else if (discount2.checked && discount3.checked) {
+    discount = Number(slider.value) * 2 + Number(slider.value) * 3;
+  } else if (discount1.checked) {
+    discount = slider.value;
+  } else if (discount2.checked) {
+    discount = slider.value * 2;
+  } else if (discount3.checked) {
+    discount = slider.value * 3;
+  } else {
+    discount = 0;
+  }
+  let finalPrice = Number(slider.value * 10) - discount;
+  calculatorPrice.innerHTML = `${finalPrice}$`;
+  suggestions.innerHTML = `${slider.value}`;
 }
 discount1.addEventListener("change", e => {
-  if (e.target.checked) {
-    finalPrice = Number(slider.value * 10) - Number(slider.value);
-    calculatorPrice.innerHTML = `${finalPrice}$`;
-  } else {
-    let finalPrice = Number(slider.value * 10);
-    calculatorPrice.innerHTML = `${finalPrice}$`;
-    priceFunction(firstDiscountValue);
-  }
+  priceFunction();
 });
-
-/* discount3.addEventListener('change',e => {
-  if(e.target.checked) {
-    calculatorPrice.innerHTML = 7;
-  }
-}); */
-
-/* const discountArray = [discount1, discount2, discount3];
-const discountValuesArray = [
-  firstDiscountValue,
-  secondDiscountValue,
-  thirdDiscountValue
-];
-//not used now
-const discountObj = [
-  { discount1: firstDiscountValue },
-  { discount2: secondDiscountValue },
-  { discount3: thirdDiscountValue }
-];
-// 3 discount functions for each checkbox, to be changed to one function using discountobj
-function discount1f(disc) {
-  disc.addEventListener("change", e => {
-    if (e.target.checked) {
-      testing(firstDiscountValue);
-    }
-    if (e.target.checked == false) {
-      calculatorPrice.innerHTML = `${slider.value}$`;
-    }
-  });
-}
-function discount2f(disc) {
-  disc.addEventListener("change", e => {
-    if (e.target.checked) {
-      testing(secondDiscountValue);
-    }
-    if (e.target.checked == false) {
-      calculatorPrice.innerHTML = `${slider.value}$`;
-    }
-  });
-}
-function discount3f(disc) {
-  disc.addEventListener("change", e => {
-    if (e.target.checked) {
-      testing(thirdDiscountValue);
-    }
-    if (e.target.checked == false) {
-      calculatorPrice.innerHTML = `${slider.value}$`;
-    }
-  });
-}
-discount1f(discount1);
-discount2f(discount2);
-discount3f(discount3);
- */
+discount2.addEventListener("change", e => {
+  priceFunction();
+});
+discount3.addEventListener("change", e => {
+  priceFunction();
+});
